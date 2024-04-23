2da33775de92b20b2ef10cbd93af175729bf464c
N3s4a <nesakrad56@gmail.com>
2024-04-23 09:30:08 +0200

    męczenie się z backend

--- registration.js
+++ registration.js
@@ -1,25 +1,54 @@
-const fs = require('fs');
+import mysql from "mysql";
 
-// Pobierz dane z formularza
-const username = document.querySelector('#username').value;
-const email = document.querySelector('#email').value;
-const password = document.querySelector('#password').value;
-
-// Utwórz obiekt z danymi
-const data = {
-    username: username,
-    email: email,
-    password: password
-};
-
-// Przekonwertuj obiekt na format JSON
-const jsonData = JSON.stringify(data);
-
-// Zapisz dane do pliku
-fs.writeFile('baza.json', jsonData, (err) => {
-    if (err) {
-        allert.error('Wystąpił błąd:', err);
-    } else {
-        allert.log('Dane zostały zapisane, możesz się zalogować');
-    }
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'Baza_danych'
 });
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database!');
+});
+
+// Get the button and form inputs
+const button = document.querySelector('#submit-button');
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const repeatPasswordInput = document.querySelector('#repeat-password');
+
+button.addEventListener('click', () => {
+  const username = usernameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const repeatPassword = repeatPasswordInput.value;
+
+  // Check if passwords match
+  if (password != repeatPassword) {
+    alert('Passwords do not match!');
+    return;
+  }
+
+  // Check if the user already exists
+  connection.query('SELECT * FROM uzytkownicy WHERE email = ?', [email], function (error, results, fields) {
+    if (error) throw error;
+
+    // If the user does not exist, insert the new user
+    if (results.length === 0) {
+      const user = { username: username, email: email, password: password };
+      connection.query('INSERT INTO uzytkownicy SET ?', user, (err, res) => {
+        if (err) throw err;
+        console.log('User added to the database');
+      });
+    } else {
+      console.log('User already exists');
+    }
+  });
+});
+
+// Close the connection
+connection.end();
