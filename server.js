6d12716c9593e19556ab3d34762b354ce6a32177
Bondarenko Anastasiia <tasyabondarenko840@gmail.com>
2024-05-15 14:47:14 +0200

    add node.js, I need coffee

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const path = require('path');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'Kalendarz/Baza_danych'
+app.get('/login', (req, res) => {
+    app.use(express.static(path.join(__dirname, 'public')));
+    res.render("log.ejs");
 });
 
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
-});
-
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+app.listen(3000);
